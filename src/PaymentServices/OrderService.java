caa33d5047ba117fdca8055a9252c9f0500caa0c
Lover1975 <drjoenh1983@gmail.com>
2024-07-18 05:42:26 -0700

    Adding related functions to the interface.

--- src/PaymentServices/OrderService.java
+++ src/PaymentServices/OrderService.java
@@ -3,7 +3,9 @@ package PaymentServices;
 public interface OrderService {
     public void onSiteOrderRegister(String customerName);
     public void onlineOrderRegister(String customerName);
+    public void phoneOrderRegister(String customerName); // New method for phone orders
+
     public void onSiteOrderPayment(int foodPrice);
     public void onlineOrderPayment(int foodPrice);
-
+    public void phoneOrderPayment(int foodPrice); // New method for phone orders
 }
