2ae35b1de8b8fa6e98728703828c2de6965a2830
Lover1975 <drjoenh1983@gmail.com>
2024-07-18 05:45:09 -0700

    Updating OnSite Order Service code.

--- src/PaymentServices/OnSiteOrderService.java
+++ src/PaymentServices/OnSiteOrderService.java
@@ -12,6 +12,12 @@ public class OnSiteOrderService implements OrderService {
     }
 
     @Override
+    public void phoneOrderRegister(String customerName) {
+        // Empty Body
+    }
+
+
+    @Override
     public void onSiteOrderPayment(int foodPrice) {
         System.out.println("on-Site Payment with Price : " + foodPrice + " Tomans!");
     }
@@ -19,5 +25,10 @@ public class OnSiteOrderService implements OrderService {
     @Override
     public void onlineOrderPayment(int foodPrice) {
         //Empty Body!
+    }
+
+    @Override
+    public void phoneOrderPayment(int foodPrice) {
+        // Empty Body
     }
 }
